40fbe95e3bebc2ecb7e60f235f38af78aa0dfb46
Nitikan Crwx <nitikan@live.com>
2017-11-30 04:13:01 +0700

    pacman can move

---
+++ desktop/src/com/crwx/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.crwx.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.crwx.mygdx.game.PacmanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+        config.height = 800;
+		new LwjglApplication(new PacmanGame(), config);
+	}
+}
